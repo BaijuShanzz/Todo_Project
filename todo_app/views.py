894bb47b5decd5a4ab5ead526cf6fded70cbf46d
Baiju shanzz <baijushanzz@gmail.com>
2023-10-08 12:11:57 +0530

    create section has to be completed

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 from todo_app.forms import TodoForm
+from todo_app.models import todolist
 
 # Create your views here.
 
 
 def index(request):
     form = TodoForm()
-    return render(request,"index.html",{'form': form})
+   
+    # submiting data
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        # checking validation
+        if form.is_valid():
+            # save value on data base
+            form.save()
+            # redirect page
+            return redirect('index')
+    # fetching data from database
+    todos = todolist.objects.all()
+    
+    return render(request,"index.html",{'form': form, 'todos':todos})
 
     
